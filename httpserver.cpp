7f3f6b0757a79195425f1a47d0671d0ae1edd0b8
Lars Hall <fartzzz@gmail.com>
2012-02-07 23:17:00 +0100

    Added support for fileloading and request types

--- httpserver.cpp
+++ httpserver.cpp
@@ -1,13 +1,15 @@
 #include "httpserver.h"
-
+#include "webfrontend.h"
 /*
  * A simple blocking httpserver
  * Uhh keep it safe behind a firewall or something
  */
-HttpServer::HttpServer(uint16_t port)
+HttpServer::HttpServer(uint16_t port, const string &serverPath)
 {
     this->port = port;
     started = false;
+    this->serverPath = serverPath;
+    this->frontend = new WebFrontend();
 }
 
 void HttpServer::startServer()
@@ -49,34 +51,98 @@ void HttpServer::run()
         socklen_t clientLen = sizeof(cliAddr);
         int newsockfd = accept(sockfd, (struct sockaddr *) &cliAddr,  &clientLen);
         memset(buffer, 0, HTTP_MAX_BUFFER);
+
         if (newsockfd)
         {
             int numbytes = read(newsockfd, buffer, HTTP_MAX_BUFFER);
             if (numbytes > 0)
             {
-                parseRequest(buffer);
-                string html = processRequest();
-                send(newsockfd, html.c_str(), html.length(), 0);
+                string path = "";
+                HttpServer::RequestType type = parseRequest(buffer, path);
+                string html = "";
+
+                // FIXME: not pretty. use bytes no matter what
+                if (!frontend->handleRequest(html, type, path))
+                {
+                    // try to see there is a file in serverpath to send
+                    uint8_t buffer[HTTP_MAX_FILE_SIZE];
+                    memset(buffer, 0, HTTP_MAX_FILE_SIZE);
+                    int size = loadFile(buffer, path);
+                    send(newsockfd, buffer, size, 0);
+
+                }
+                else
+                    send(newsockfd, html.c_str(), html.length(), 0);
                 close(newsockfd);
             }
         }
     }
 }
 
-string HttpServer::processRequest()
+HttpServer::RequestType HttpServer::parseRequest(
+    const string &req, string &path)
 {
-    string ret = "HTTP/1.0 0 OK\r\n\r\n";
-    ret += "<html><body><b>HEJ</b></body></html>\r\n";
+    char tmp[HTTP_MAX_TOKEN];
+    string tokens[2];
+    int numTokens = 0;
+    RequestType ret = HTTP_UNKNOWN;
+
+    strncpy(tmp, req.c_str(), HTTP_MAX_TOKEN);
+    char *token = strtok (tmp, " ");
+    path = "";
+
+    while (token != NULL)
+    {
+        tokens[numTokens ++] = token;
+        // Only request type and path are relevant
+        if (numTokens >= 2)
+            break;
+
+        token = strtok (NULL, " ");
+    }
+
+    if (numTokens == 2)
+    {
+        if (tokens[0] == "GET")
+            ret = HTTP_GET;
+        else if (tokens[0] == "POST")
+            ret = HTTP_POST;
+        else if (tokens[0] == "PUT")
+            ret = HTTP_PUT;
+        else if (tokens[0] == "DELETE")
+            ret = HTTP_DELETE;
+
+
+        fprintf(stderr, "RequestType:%s\n", tokens[0].c_str());
+        fprintf(stderr, "Path:%s\n", tokens[1].c_str());
+        path = tokens[1];
+
+    }
 
     return ret;
 }
 
-void HttpServer::parseRequest(const string &req)
+int HttpServer::loadFile(uint8_t *bytes, const string &filename)
 {
+    int ret = -1;
+    ifstream file((serverPath + filename).c_str(),
+        ios::in|ios::binary|ios::ate);
 
+    if (file.is_open())
+    {
+        size_t size = file.tellg();
+        file.seekg(0, ios::beg);
+        file.read((char*)bytes, size);
+        file.close();
+        ret = true;
+        ret = size;
+    }
+
+    return ret;
 }
+
 
 HttpServer::~HttpServer()
 {
-
+    delete frontend;
 }
--- httpserver.h
+++ httpserver.h
@@ -9,25 +9,47 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <string>
+#include <fstream>
+#include <sys/stat.h>
 #include "thread.h"
 
 #define HTTP_MAX_PENDING 10
 #define HTTP_MAX_BUFFER 1024
+#define HTTP_MAX_TOKEN 1024
+#define HTTP_MAX_FILE_SIZE 100000
 
 using std::string;
+using std::ifstream;
+using std::ios;
+
+class WebFrontend;
+
 
 class HttpServer : public Thread
 {
+    public:
+        enum RequestType
+        {
+            HTTP_GET = 0,
+            HTTP_POST,
+            HTTP_DELETE,
+            HTTP_PUT,
+            HTTP_UNKNOWN
+        };
+
     private:
+        WebFrontend *frontend;
         bool started;
         int sockfd;
         short port;
         void startServer();
-        string processRequest();
-        void parseRequest(const string &req);
+        RequestType parseRequest(const string &req, string &path);
+        string serverPath;
+        // returns -1 if file not found or cannot read
+        int loadFile(uint8_t *bytes, const string &filename);
 
     public:
-        HttpServer(uint16_t port);
+        HttpServer(uint16_t port, const string &serverPath);
         ~HttpServer();
         void run();
 };
