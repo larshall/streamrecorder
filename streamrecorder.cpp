30c6c4e66bf943e633232ad23db2c808741600b3
Lars Hall <fartzzz@gmail.com>
2012-02-13 17:35:00 +0100

    Added callbacks for getProgrammes getChannels etc. (Right now only used by http server)

--- streamrecorder.cpp
+++ streamrecorder.cpp
@@ -2,10 +2,23 @@
 #include "h264.h"
 #include "streamrecorder.h"
 #include "recorder.h"
+#include "webfrontend.h"
 
 int main()
 {
-    HttpServer server(8182, "./data");
+    /*
+    XmlTv xml;
+    if (xml.loadFile("data/tv.xml"))
+    {
+        fprintf(stderr, "Loaded file\n");
+        xml.readChannels();
+    }
+    */
+    StreamRecorder streamRecorder;
+    WebFrontend frontend(&streamRecorder);
+    //streamRecorder.getProgrammes("3plus");
+
+    HttpServer server(8182, "./data", &frontend);
     server.start();
     while(true)
     {
@@ -27,4 +40,25 @@ int main()
 StreamRecorder::StreamRecorder()
 {
     numRecordings = 0;
+    xmltv.loadFile("data/tv.xml", "da");
+}
+
+void StreamRecorder::getChannels(vector<Channel> &channels)
+{
+    ScopedLock lock(&mutex);
+    xmltv.readChannels(channels);
+}
+
+void StreamRecorder::getProgrammes(const string &channelId,
+    vector<Programme> &programmes)
+{
+    ScopedLock lock(&mutex);
+    xmltv.readProgrammes(channelId, programmes);
+}
+
+void StreamRecorder::getProgramme(const string &channelId,
+    const string &start, Programme &programme)
+{
+    ScopedLock lock(&mutex);
+    xmltv.readProgramme(channelId, start, programme);
 }
--- streamrecorder.h
+++ streamrecorder.h
@@ -1,7 +1,9 @@
 #ifndef STREAMRECORDER_H
 #define STREAMRECORDER_H
 
+#include "xmltv.h"
 #include "httpserver.h"
+#include "thread.h"
 
 #define DEBUG
 
@@ -9,8 +11,15 @@ class StreamRecorder
 {
     private:
         int numRecordings;
+        XmlTv xmltv;
+        Mutex mutex;
     public:
         StreamRecorder();
+        void getChannels(vector<Channel> &channels);
+        void getProgrammes(const string &channelId,
+            vector<Programme> &programmes);
+        void getProgramme(const string &channelId,
+            const string &start, Programme &programme);
 };
 
 #endif
