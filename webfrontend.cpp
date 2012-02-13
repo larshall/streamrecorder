a08d0cd9db30cd384f1d933f710a471f574109a3
Lars Hall <fartzzz@gmail.com>
2012-02-13 17:35:40 +0100

    Added support for getprogrammes request

--- webfrontend.cpp
+++ webfrontend.cpp
@@ -1,38 +1,138 @@
 #include "webfrontend.h"
+#include "streamrecorder.h"
 
-WebFrontend::WebFrontend()
-{}
+WebFrontend::WebFrontend(StreamRecorder *streamRecorder)
+{
+    this->streamRecorder = streamRecorder;
+}
 
 int WebFrontend::handleRequest(string &contentType, uint8_t *bytes,
-    HttpServer::RequestType type, const string &req)
+    Request &request)
 {
     int ret = -1;
     bool found = false;
-    string html = "";
+    string output = "";
 
-    if (req == "/")
+    // gets all channels
+    if (request.path == "/get-channels")
     {
-        html += "<html><body>streamrecorder</body></html>";
-        found = true;
-       
-    }
-    if (req == "/test")
-    {
-        html += "<html><body><b>TEST</b><img src=\"./logo.png\" /></body></html>";
+        getChannels(contentType, output, request);
         found = true;
     }
-    else if (req == "/get-channels")
+    // gets a list of programmes for a specific channel
+    else if (request.path == "/get-programmes")
     {
-        contentType = "application/json";
-        html += "{ \"id\" : \"DR1\" , \"type\" : \"FILM\" }";
+        getProgrammes(contentType, output, request);
+        found = true;
+    }
+    // gets a specfic programme for a specific channel and time
+    else if (request.path == "/get-programme")
+    {
+        getProgramme(contentType, output, request);
         found = true;
     }
 
     if (found)
     {
-        ret = html.length();
-        memcpy(bytes, html.c_str(), html.length());
+        ret = output.length();
+        memcpy(bytes, output.c_str(), output.length());
     }
 
     return ret;
 }
+
+void WebFrontend::getChannels(string &contentType, string &output,
+    Request &request)
+{
+    contentType = "application/json";
+    vector<Channel> channels;
+    stringstream ss;
+    streamRecorder->getChannels(channels);
+    ss << "{\"Channels\" : [";
+    for (unsigned int i = 0; i < channels.size(); i++)
+    {
+        ss << " { ";
+        ss << "\"id\" : \"" + channels[i].id + "\" , ";
+        ss << "\"displayname\" : \"" + channels[i].displayName + "\"";
+        ss << " } ";
+        // TODO: create a general vector->json array function
+        if (i != channels.size() - 1)
+            ss << ",";
+    }
+    ss << "]}";
+
+    output = ss.str();
+}
+
+void WebFrontend::getProgrammes(string &contentType, string &output,
+    Request &request)
+{
+    string channelId = "";
+    HttpServer::RequestParams::const_iterator it;
+    contentType = "application/json";
+
+    for (it = request.params.begin(); it != request.params.end(); it ++)
+    {
+        if (it->first == "channelid")
+            channelId = it->second;
+    }
+
+    vector<Programme> programmes;
+    stringstream ss;
+    streamRecorder->getProgrammes(channelId, programmes);
+    ss << "{\"Programmes\" : [";
+    for (unsigned int i = 0; i < programmes.size(); i++)
+    {
+        ss << " { ";
+        ss << "\"start\" : \"" + programmes[i].start + "\" , ";
+        ss << "\"title\" : \"" + programmes[i].title[0].second + "\"";
+        ss << " } ";
+        // TODO: create a general vector->json array function
+        if (i != programmes.size() - 1)
+            ss << ",";
+    }
+    ss << "]}";
+
+    output = ss.str();
+}
+
+void WebFrontend::getProgramme(string &contentType, string &output,
+    Request &request)
+{
+    string channelId = "";
+    string start = "";
+    HttpServer::RequestParams::const_iterator it;
+    contentType = "application/json";
+
+    for (it = request.params.begin(); it != request.params.end(); it ++)
+    {
+        if (it->first == "channelid")
+            channelId = it->second;
+        if (it->first == "start")
+            start = it->second;
+    }
+
+    // Hack: the httpserver doesn't support urldecode yet,
+    // so space in iso8601 extended date is removed
+    int pos = start.find("+");
+    start.insert(pos, " ");
+
+    Programme programme;
+    stringstream ss;
+    streamRecorder->getProgramme(channelId, start, programme);
+    ss << "{\"Programme\" : [";
+    ss << " { ";
+    ss << "\"start\" : \"" + programme.start + "\" , ";
+    if ((programme.title.size() > 0) && (programme.description.size() > 0))
+    {
+        ss << "\"title\" : \"" + programme.title[0].second + "\"";
+        ss << "\"description\" : \"" + programme.description[0].second + "\"";
+    }
+    ss << " } ";
+    // TODO: create a general vector->json array function
+    ss << "]}";
+
+    output = ss.str();
+
+}
+
--- webfrontend.h
+++ webfrontend.h
@@ -2,17 +2,30 @@
 #define WEBFRONTEND_H
 
 #include <string>
+#include <sstream>
 #include "httpserver.h"
 
 using std::string;
+using std::stringstream;
+
+class StreamRecorder;
 
 class WebFrontend
 {
+    private:
+        StreamRecorder *streamRecorder;
+
+        void getChannels(string &contentType, string &output,
+            Request &request);
+        void getProgrammes(string &contentType, string &output,
+            Request &request);
+        void getProgramme(string &contentType, string &output,
+            Request &request);
     public:
-        WebFrontend();
+        WebFrontend(StreamRecorder *streamRecorder);
         // returns the size (>0) if a page is found
         int handleRequest(string &contentType, uint8_t *bytes,
-            HttpServer::RequestType type, const string &req);
+            Request &requst);
 };
 
 #endif
