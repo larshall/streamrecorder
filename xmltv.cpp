14e81893e3dc5855dd89af4d3feda01cfbd10f30
Lars Hall <fartzzz@gmail.com>
2012-02-13 17:36:11 +0100

    Added support for querying channels and programmes

--- xmltv.cpp
+++ xmltv.cpp
@@ -1,1 +1,67 @@
 #include "xmltv.h"
+
+bool XmlTv::loadFile(const string &filename, const string &lang)
+{
+    this->lang = lang;
+    pugi::xml_parse_result res = doc.load_file(filename.c_str());
+    if (!res)
+    {
+        fprintf(stderr, "Error: Cannot loadfile: %s\n", filename.c_str());
+        return false;
+    }
+
+    return true;
+}
+
+void XmlTv::readChannels(vector<Channel> &channels)
+{
+    pugi::xml_node channelsNode = doc.child("tv");
+    for (pugi::xml_node channelNode = channelsNode.child("channel");
+        channelNode; channelNode = channelNode.next_sibling("channel"))
+    {
+          Channel c;
+          c.id = channelNode.attribute("id").value();
+          c.displayName = channelNode.child("display-name").child_value();
+          channels.push_back(c);
+    }
+}
+
+void XmlTv::readProgrammes(const string &channelId,
+    vector<Programme> &programmes)
+{
+    string query  = "/tv/programme[@channel = '" + channelId + "']";
+    pugi::xpath_node_set tools = doc.select_nodes(query.c_str());
+    for (pugi::xpath_node_set::const_iterator it = tools.begin();
+        it != tools.end(); ++it)
+    {
+        pugi::xpath_node node = *it;
+        string start = node.node().attribute("start").value();
+        string title = node.node().child("title").child_value();
+        Programme p;
+        p.start = start;
+        p.title.push_back(make_pair("da", title));
+        programmes.push_back(p);
+    }
+}
+
+void XmlTv::readProgramme(const string &channelId,
+    const string &start, Programme &programme)
+{
+    string query  = "/tv/programme[@channel = '" + channelId +
+        "' and @start = '" + start + "']";
+
+    std::cout << "\nQuery:" << query << std::endl;
+    pugi::xpath_node_set tools = doc.select_nodes(query.c_str());
+    for (pugi::xpath_node_set::const_iterator it = tools.begin();
+        it != tools.end(); ++it)
+    {
+        pugi::xpath_node node = *it;
+        string start = node.node().attribute("start").value();
+        string title = node.node().child("title").child_value();
+        string description = node.node().child("description").child_value();
+
+        programme.start = start;
+        programme.title.push_back(make_pair("da", title));
+        programme.description.push_back(make_pair("da", description));
+    }
+}
--- xmltv.h
+++ xmltv.h
@@ -1,10 +1,57 @@
 #ifndef XMLTV_H
 #define XMLTV_H
 
+#include <vector>
+#include <string>
+#include <stdio.h>
+#include <iostream>
+// PugiXML now supports xpaths with exception disabled
+#include "pugixml/pugixml.hpp"
+
+using std::string;
+using std::vector;
+using std::pair;
+using std::make_pair;
+using std::cout;
+using std::endl;
+
+struct Channel
+{
+    string id;
+    string displayName;
+};
+
+struct Programme
+{
+    // TODO: convert to datetime
+    string start;
+    string channeldId;
+    // lang, text
+    vector<pair<string, string> > title;
+    vector<pair<string, string> > description;
+    vector<pair<string, string> > categories;
+};
+
+/**
+ * XmlTv reads a xmltv file.
+ * TODO: buffer some of the information istead of
+ * parsing the xml file every time
+ */
 class XmlTv
 {
+    private:
+        pugi::xml_document doc;
+        string lang;
+
     public:
-        XmlTv();
+        XmlTv(){};
+        bool loadFile(const string &filename, const string &lang);
+        void readChannels(vector<Channel> &channels);
+        void readProgrammes(const string &channelId,
+            vector<Programme> &programmes);
+        void readProgramme(const string &channelId,
+            const string &start, Programme &programme);
+
 };
 
 #endif
