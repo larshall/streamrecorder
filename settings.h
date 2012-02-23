1105c1b664fe77f65fb3c7682a934d3ce552f298
Lars Hall <fartzzz@gmail.com>
2012-02-23 18:37:37 +0100

    Added settings page for setting up channelstreams

--- settings.h
+++ settings.h
@@ -1,12 +1,40 @@
 #ifndef SETTINGS_H
 #define SETTINGS_H
 
+#include <string>
+#include <vector>
+#include <sstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 #include "pugixml/pugixml.hpp"
+
+using std::string;
+using std::vector;
+using std::stringstream;
+
+struct ChannelStream
+{
+    string channel;
+    string host;
+    uint16_t port;
+};
 
 class Settings
 {
+    private:
+        pugi::xml_document doc;
+        bool readSettings();
+        string filename;
+
     public:
+        vector<ChannelStream> channelStreams;
+
         Settings();
+        bool load(const string &filename);
+        bool reload();
+        bool save();
+
 };
 
 #endif
