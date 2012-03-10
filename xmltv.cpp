5d43ca97f0063aa239d13c4e7eadc51ada489ae2
Lars Hall <fartzzz@gmail.com>
2012-03-11 00:06:18 +0100

    Added GPL license header

--- xmltv.cpp
+++ xmltv.cpp
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2012 Lars Hall
+ *
+ * This file is part of StreamRecorder.
+ *
+ * StreamRecorder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * StreamRecorder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with StreamRecorder.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "xmltv.h"
 
 bool XmlTv::loadFile(const string &filename, const string &lang)
